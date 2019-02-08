5c16080ee35e5c6d1804afc62d11f4e9b6cd036e
Oliver Ranson <Oliver.Ranson@firsthelpline.com>
2019-02-08 17:55:37 +0000

    working api on localhost

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -3,5 +3,5 @@ module.exports = function(app)
     let api = require('../controllers/controllers');
 
     app.route('/digits')
-        .get(api.get_all_words)
+        .post(api.get_all_words)
 };
--- server.js
+++ server.js
@@ -1,7 +1,13 @@
-var express = require('express');
-app = express();
-port = 3000;
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/routes');
+
+routes(app);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
