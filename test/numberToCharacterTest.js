df1317eb6859f64b7b0b411636c0ac30f99899f5
Oliver Ranson <Oliver.Ranson@firsthelpline.com>
2019-02-08 18:12:28 +0000

    converts 2 to a, b, c

---
+++ test/numberToCharacterTest.js
@@ -0,0 +1,24 @@
+var assert = require("chai").assert;
+
+describe("converts numbers to letters", () => {
+  it("should convert 2", () => {
+    var two = "2";
+
+    assert.deepEqual(numberToLetters(two), ["a", "b", "c"]);
+  });
+});
+
+function numberToLetters(numberAsText) {
+  return map[numberAsText];
+}
+
+const map = {
+  "2": ["a", "b", "c"],
+  "3": ["d", "e", "f"],
+  "4": ["g", "h", "i"],
+  "5": ["j", "k", "l"],
+  "6": ["m", "n", "o"],
+  "7": ["p", "q", "r", "s"],
+  "8": ["t", "u", "v"],
+  "9": ["w", "x", "y", "z"]
+};
