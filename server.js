0d3199e2371419169e915c7d53e1998710adc4f7
Olly <oranson1@gmail.com>
2019-02-10 11:03:07 +0000

    allows local host 8080 to use api

--- server.js
+++ server.js
@@ -2,9 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+const cors = require('cors');
+
+const corsOptions = {
+  origin: 'http://localhost:8080',
+  optionsSuccessStatus: 200
+}
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors(corsOptions));
 
 var routes = require('./api/routes/routes');
 
