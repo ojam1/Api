93cfa82a8c3d3b58819b148b04508785af93249c
Oliver Ranson <Oliver.Ranson@firsthelpline.com>
2019-02-12 14:55:35 +0000

    sets up api for heroku

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
-const cors = require('cors');
+const port = process.env.PORT || 3000;
 
-const corsOptions = {
-  origin: 'http://localhost:8080',
-  optionsSuccessStatus: 200
-}
+const allowCrossDomain = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'POST');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors(corsOptions));
+app.use(allowCrossDomain);
 
 const routes = require('./api/routes/routes');
 
 routes(app);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-
+app.listen(port)
